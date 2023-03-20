2dc227de88c9bea0fea9bf1f2c0447b857a7f08d
rodrigo quintero <rodrigo.m.quintero@gmail.com>
2023-03-20 10:43:29 -0300

    :zap: corrigiendo mailer

--- escuela_backend/lib/services/mailer_service.dart
+++ escuela_backend/lib/services/mailer_service.dart
@@ -3,58 +3,46 @@ import 'dart:io';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 
-import '../utility/mailer/templates/calification_template.dart';
+class mailer {
+  sendMailerFuncion(String ccRecipient,
+      {required String mailDestinatario, required String mailHtml}) async {
+    //TODO: pasar a variables de entorno (dotenv)
+    String username = 'nidus.escuelas@gmail.com';
+    String password = 'uoqmujlachjpsxkt';
+    final finalccRecipients = ccRecipient ?? 'nidus.escuelas@gmail.com';
 
-void main(List<String> args) async {
-  await mailer(
-      mailDestinatario: 'lucas.olimpi@gmail.com',
-      name: 'Rodrigo',
-      lastname: 'Quintero');
-}
+    final smtpServer = gmail(username, password);
 
-final mailHtml =
-    mailCalificacions(nombreDelTutor: "juan perez", nombreAlumno: "josesito");
+    //TODO: crear mensaje con datos de la base de datos
+    final message = Message()
+      ..from = Address(username, 'Nidus Escuelas')
+      ..recipients.add('$mailDestinatario')
+      ..ccRecipients.addAll(['$finalccRecipients'])
+      ..subject = 'Calificaciones !!! :: 😀 :: ${DateTime.now()}'
+      ..html = mailHtml;
 
-mailer(
-    {required String mailDestinatario,
-    required String name,
-    required String lastname}) async {
-  //TODO: pasar a variables de entorno (dotenv)
-  String username = 'nidus.escuelas@gmail.com';
-  String password = 'uoqmujlachjpsxkt';
+    // ..attachments = [
+    //   FileAttachment(File(''))
+    //     ..location = Location.inline   //todo: revisar envio de adjuntos
+    //     ..cid = '<myimg@3.141>'
+    // ];
 
-  final smtpServer = gmail(username, password);
-
-  //TODO: crear mensaje con datos de la base de datos
-  final message = Message()
-    ..from = Address(username, 'Nidus Escuelas')
-    ..recipients.add('$mailDestinatario')
-    ..ccRecipients.addAll(['rodrigo.m.quintero@gmail.com'])
-    ..subject = 'su hijo ya tiene notas !!! :: 😀 :: ${DateTime.now()}'
-    ..text = 'This is the plain text.\nThis is line 2 of the text part.'
-    ..html = mailHtml;
-
-  // ..attachments = [
-  //   FileAttachment(File(''))
-  //     ..location = Location.inline   //todo: revisar envio de adjuntos
-  //     ..cid = '<myimg@3.141>'
-  // ];
-
-  try {
-    final sendReport = await send(message, smtpServer);
-    print('Message sent: ' + sendReport.toString());
-  } on MailerException catch (e) {
-    print('Message not sent.');
-    for (var p in e.problems) {
-      print('Problem: ${p.code}: ${p.msg}');
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
     }
+
+    var connection = PersistentConnection(smtpServer);
+
+    // Send the first message
+    await connection.send(message);
+
+    // close the connection
+    await connection.close();
   }
-
-  var connection = PersistentConnection(smtpServer);
-
-  // Send the first message
-  await connection.send(message);
-
-  // close the connection
-  await connection.close();
 }
