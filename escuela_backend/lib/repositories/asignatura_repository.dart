3cf0416b48f74cc72d9754f906e1157213676642
rodrigo quintero <rodrigo.m.quintero@gmail.com>
2023-03-23 08:21:22 -0300

    :hotfix: corregidas las funciones, se agrego tipado a las devoluciones

--- escuela_backend/lib/repositories/asignatura_repository.dart
+++ escuela_backend/lib/repositories/asignatura_repository.dart
@@ -7,24 +7,15 @@ class AsignaturaRepository {
   final client = SupabaseClient('https://rlsbqzngalrkrzijpnjy.supabase.co',
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsc2Jxem5nYWxya3J6aWpwbmp5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3Mzc5ODM5NSwiZXhwIjoxOTg5Mzc0Mzk1fQ.1Z_7PSi0duNogmYUrvb7QfnJgtqt37A3qK6dYOCGTmk');
 
-  getAsignaturaById(idAsignatura) async {
-    try {
-      final response = await client
-          .from('Asignatura')
-          .select('*, docente(*)')
-          .eq('idAsignatura', idAsignatura)
-          .execute();
-      if (response.error != null) {
-        return {
-          "error": response.error?.message,
-        };
-      } else {
-        return response.data[0];
-      }
-    } catch (e) {
-      return {
-        "error": e,
-      };
+  Future<Map> getAsignaturaById(idAsignatura) async {
+    final response = await client
+        .from('Asignatura')
+        .select('*, docente(*)')
+        .eq('idAsignatura', idAsignatura)
+        .execute();
+    if (response.error != null) {
+      throw Exception(response.error!.message);
     }
+    return response.data[0];
   }
 }
