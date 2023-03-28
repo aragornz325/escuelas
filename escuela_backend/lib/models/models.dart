ea185e3adf306119ee4a0c45e29b35202c90fef4
johnnie <juanjose.garcia98@yahoo.com.ar>
2023-03-28 09:23:36 -0300

    🚧 Estructura de directorios y archivos de modelos.

--- escuela_backend/lib/models/models.dart
+++ escuela_backend/lib/models/models.dart
@@ -1,2 +1,8 @@
 // Archivo desde el que se accede a todos los modelos.
-export 'user_model.dart';
+export 'alumno/alumno_model.dart';
+export 'asignatura/asignatura_model.dart';
+export 'calificacion/calificacion_model.dart';
+export 'curso/curso_model.dart';
+export 'docente/docente_model.dart';
+export 'enlace/enlace_model.dart';
+export 'operacion/operacion_model.dart';
