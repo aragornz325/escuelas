9242ac4d2882567917031ec077aa62e70e23601c
rodrigo quintero <rodrigo.m.quintero@gmail.com>
2023-03-28 11:54:21 -0300

    :construction: trabajando en la logica de agrupacion de calificaciones por mail

--- escuela_backend/lib/repositories/calificaciones_repository.dart
+++ escuela_backend/lib/repositories/calificaciones_repository.dart
@@ -41,4 +41,119 @@ class CalificacionesRepository extends Repository {
     }
     return response.data;
   }
+
+  Future<List> getCalificacionesByPeriodo({required String periodo}) async {
+    final response =
+        await client.from('Nota').select().eq('periodo', periodo).execute();
+    if (response.error != null) {
+      throw Exception(response.error!.message);
+    }
+    if (response.data.isEmpty) {
+      throw Response.found('aun no hay calificaciones en este periodo');
+    }
+    return response.data;
+  }
+
+  /// get notas by idDocente, populate with alumno and materia
+  Future<List<Map>> getCalificacionesByDocente(
+      {required String idDocente}) async {
+    final response = await client
+        .from('Nota')
+        .select(
+            'nota, Alumno:idAlumno(nombre, apellido, email), Asignatura:idAsignatura(nombre), Curso:idCurso(nombre)')
+        .eq('idDocente', idDocente)
+        .execute();
+    if (response.error != null) {
+      throw Exception(response.error!.message);
+    }
+    final notasByAlumno = <String, Map<String, dynamic>>{};
+
+    for (final nota in response.data) {
+      final nombreAlumno = nota['Alumno']['nombre'] as String;
+
+      // ignore: avoid_single_cascade_in_expression_statements
+      notasByAlumno.putIfAbsent(
+          nombreAlumno,
+          () => {
+                'nombre': nombreAlumno,
+                'apellido': nota['Alumno']['apellido'],
+                'email': nota['Alumno']['email'],
+                'notas': [],
+              })
+        ..['notas'].add({
+          'asignatura': nota['Asignatura']['nombre'],
+          'curso': nota['Curso']['nombre'],
+          'nota': nota['nota'],
+        });
+    }
+
+    return notasByAlumno.values.toList();
+  }
+
+  Future<List<Map>> getCalificacionesByCurso({required String idCurso}) async {
+    final response = await client
+        .from('Nota')
+        .select(
+            'nota, Alumno:idAlumno(nombre, apellido, email), Asignatura:idAsignatura(nombre), Curso:idCurso(nombre)')
+        .eq('idCurso', idCurso)
+        .execute();
+    if (response.error != null) {
+      throw Exception(response.error!.message);
+    }
+    final notasByAlumno = <String, Map<String, dynamic>>{};
+
+    for (final nota in response.data) {
+      final nombreAlumno = nota['Alumno']['nombre'] as String;
+
+      // ignore: avoid_single_cascade_in_expression_statements
+      notasByAlumno.putIfAbsent(
+          nombreAlumno,
+          () => {
+                'nombre': nombreAlumno,
+                'apellido': nota['Alumno']['apellido'],
+                'email': nota['Alumno']['email'],
+                'notas': [],
+              })
+        ..['notas'].add({
+          'asignatura': nota['Asignatura']['nombre'],
+          'curso': nota['Curso']['nombre'],
+          'nota': nota['nota'],
+        });
+    }
+
+    return notasByAlumno.values.toList();
+  }
+
+  Future<List> getCalificacionesByidEnlace({required String idEnlace}) async {
+    final response = await client
+        .from('Nota')
+        .select(
+            'nota, Alumno:idAlumno(nombre, apellido, email), Asignatura:idAsignatura(nombre), Curso:idCurso(nombre)')
+        .eq('idEnlace', idEnlace)
+        .execute();
+    if (response.error != null) {
+      throw Exception(response.error!.message);
+    }
+    final notasByAlumno = <String, Map<String, dynamic>>{};
+    for (final nota in response.data) {
+      final nombreAlumno = nota['Alumno']['nombre'] as String;
+
+      // ignore: avoid_single_cascade_in_expression_statements
+      notasByAlumno.putIfAbsent(
+          nombreAlumno,
+          () => {
+                'nombre': nombreAlumno,
+                'apellido': nota['Alumno']['apellido'],
+                'email': nota['Alumno']['email'],
+                'notas': [],
+              })
+        ..['notas'].add({
+          'asignatura': nota['Asignatura']['nombre'],
+          'curso': nota['Curso']['nombre'],
+          'nota': nota['nota'],
+        });
+    }
+
+    return notasByAlumno.values.toList();
+  }
 }
--- escuela_backend/lib/repositories/link_repository.dart
+++ escuela_backend/lib/repositories/link_repository.dart
@@ -1,10 +1,13 @@
 import 'base_repository.dart';
 
 class LinkRepository extends Repository {
+  ///esta funcion crea un registro de un link de calificaciones
+  ///recibe el token, el periodo y el id del docente
   Future<Map> createLinkRegister(
       {required String token,
       required String periodo,
-      required String idDocente}) async {
+      required String idDocente,
+      required String idAsignatura}) async {
     final registro = {
       'idDocente': idDocente,
       'notasCargadas': false,
@@ -12,9 +15,9 @@ class LinkRepository extends Repository {
       'periodo': periodo,
       'active': true,
       'token': token,
+      'idAsignatura': idAsignatura,
     };
     final response = await client.from('Enlace').insert(registro).execute();
-    print(response.data);
 
     if (response.error != null) {
       throw Exception(response.error);
@@ -26,14 +29,20 @@ class LinkRepository extends Repository {
     return response.data[0];
   }
 
-  Future<List> getLinkByDocenteAndPeriodo(
-      {required String idDocente, required String periodo}) async {
+  ///esta funcion devuelve todos los link de calificaciones
+  ///generados para un docente y un periodo especifico
+  ///DEVUELVE SOLO LOS ACTIVOS
+  Future<List> getLinkByDocenteAndPeriodoAndAsignatura(
+      {required String idDocente,
+      required String periodo,
+      required String idAsignatura}) async {
     final response = await client
         .from('Enlace')
         .select('*, Docente(*)')
         .eq('idDocente', idDocente)
         .eq('periodo', periodo)
         .eq('active', true)
+        .eq('idAsignatura', idAsignatura)
         .execute();
 
     if (response.error != null) {
@@ -42,6 +51,8 @@ class LinkRepository extends Repository {
     return response.data;
   }
 
+  ///esta funcion devuelve un link por su token
+  ///devuelve un solo registro, sin importar su condicion active
   Future<Map> getLinkByToken(String token) async {
     final response =
         await client.from('Enlace').select().eq('token', token).execute();
@@ -52,9 +63,12 @@ class LinkRepository extends Repository {
     if (response.data.isEmpty) {
       throw Exception('No se encontro el link');
     }
+
     return response.data[0];
   }
 
+  ///esta funcion actualiza el registro de un link
+  ///recibe el id del link y el registro a actualizar
   Future<Map> updateLinkRegister(
       {required String idEnlace, required Map registro}) async {
     final response = await client
@@ -70,5 +84,22 @@ class LinkRepository extends Repository {
       throw Exception('No se pudo actualizar el registro');
     }
     return response.data[0];
+  }
+
+  ///esta funcion devuelve todos lo link de calificaciones
+  ///generados para un periodo especifico
+  ///devuelve totos, sin importar su condicion active
+  Future<List> getLinksByPeriodo(String periodo) async {
+    final response = await client
+        .from('Enlace')
+        .select('*, Docente(*)')
+        .eq('periodo', periodo)
+        .eq('active', true)
+        .execute();
+
+    if (response.error != null) {
+      throw Exception(response.error);
+    }
+    return response.data;
   }
 }
