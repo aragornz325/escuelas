b9a739975d3dcf781fef27cfea2d0d7876c1011f
johnnie <juanjose.garcia98@yahoo.com.ar>
2023-03-28 10:22:04 -0300

    Modelo de Alumno. Archivo de variables constantes: nombres de las tablas.

--- escuela_backend/lib/models/alumno/alumno_model.dart
+++ escuela_backend/lib/models/alumno/alumno_model.dart
@@ -1,13 +1,30 @@
 // Modelo para la Entidad Usuario.
-// import 'package:dart_mappable/dart_mappable.dart';
+import 'package:dart_mappable/dart_mappable.dart';
 
-// part 'user_model.mapper.dart';
+part 'alumno_model.mapper.dart';
 
-// class UserModel with UserModelMappable {
+@MappableClass(ignoreNull: true)
+class AlumnoModel with AlumnoModelMappable {
 
-//   @MappableConstructor()
-//   const UserModel({
+  // Campos del modelo.
+  String? idAlumno;
+  DateTime? createdAt;
+  String nombre;
+  String apellido;
+  String email;
+  String curso;
+  bool? egresado;
 
-//   })
+  // Version del modelo para creacion.
+  @MappableConstructor()
+  AlumnoModel.create(this.nombre, this.apellido, this.email, this.curso);
 
-// }
+  // Version del modelo para recuperar informacion.
+  @MappableConstructor()
+  AlumnoModel.get(this.idAlumno, this.createdAt, this.nombre, this.apellido, this.email, this.curso, this.egresado);
+
+  // Version del modelo para actualizacion.
+  @MappableConstructor()
+  AlumnoModel.update(this.nombre, this.apellido, this.email, this.curso);
+
+}
